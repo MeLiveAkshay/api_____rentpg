db7d657f20234083a7048984c558f654b3350475
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-06-21 21:50:56 +0530

    Add Owner model and admin configuration with initial migration

--- owner/admin.py
+++ owner/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Owner
 # Register your models here.
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone',)
+    search_fields = ('name', 'email','owner_id')
+
+admin.site.register(Owner, OwnerAdmin)
--- owner/models.py
+++ owner/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Owner(models.Model):
+    owner_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    password = models.CharField(max_length=128)  # Suitable for hashed passwords
+
+    def __str__(self):
+        return self.name
